c6087605b8b52b83b7190de381dbea9af5a81f53
fpoll <fpollmueller@googlemail.com>
2017-08-21 13:35:33 +0200

    some message

--- public/js/meeting.js
+++ public/js/meeting.js
@@ -583,7 +583,7 @@ function handleDisagreement(senderStreamId) {
 
 function handleAudioLevel(audioLevel) {
     // set audio level to meter
-    document.getElementById("audioMeterVal").value(audioLevel);
+    $("#audioMeterVal").attr("value", audioLevel);
 }
 
 // visualize, that someone used a superpower
