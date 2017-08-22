e0ae0cdba7b8f00b94a774bf59be8499292b02f7
fpoll <fpollmueller@googlemail.com>
2017-08-22 12:09:47 +0200

    some message

--- public/js/meeting.js
+++ public/js/meeting.js
@@ -920,6 +920,7 @@ function uiTimeToEnd() {
     if (m.queue.length > 0) {
         //sconsole.log(Math.floor(secondsLeft) + " seconds left");
         $("#" + talkTimeLeftUi).html(Math.floor(secondsLeft));
+        alert("sec left: " + secondsLeft + "Date Now " + Date.now());
         // if time is over
         if (secondsLeft <= 1) {
             $("#" + talkTimeLeftUi).html(`<i class="fa fa-hourglass" aria-hidden="true"></i>`);
