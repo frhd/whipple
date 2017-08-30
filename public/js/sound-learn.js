8528a2c8d1615d1567d5e93b9b48164658043c43
fpoll <fpollmueller@googlemail.com>
2017-08-30 16:14:48 +0200

    some message

---
+++ public/js/sound-learn.js
@@ -0,0 +1,9 @@
+// sounds are initialized in meeting.js
+
+$("#btn_sound-learn-start-talk").click(function() {
+    talkingSound.play();
+});
+
+$("#btn_sound-learn-end-talk").click(function() {
+    doneTalkingSound.play();
+});
