c514f1991a4bd0bad09f9119c255eb94440883b2
fpoll <fpollmueller@googlemail.com>
2017-08-30 16:18:44 +0200

    some message

--- public/js/sound-learn.js
+++ public/js/sound-learn.js
@@ -7,3 +7,23 @@ $("#btn_sound-learn-start-talk").click(function() {
 $("#btn_sound-learn-end-talk").click(function() {
     doneTalkingSound.play();
 });
+
+$("#btn_sound-learn-queue-in").click(function() {
+    queueInSound.play();
+});
+
+$("#btn_sound-learn-queue-out").click(function() {
+    queueOutSound.play();
+});
+
+$("#btn_sound-learn-join").click(function() {
+    joinSound.play();
+});
+
+$("#btn_sound-learn-alarm").click(function() {
+    alarmSound.play();
+});
+
+$("#btn_sound-learn-notification").click(function() {
+    notificationSound.play();
+});
